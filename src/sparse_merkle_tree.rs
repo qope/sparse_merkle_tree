9f1c90593fadf9967a77caf8c903a9877ad39e4c
nuno <41233686+qope@users.noreply.github.com>
2023-01-14 00:23:34 +0900

    update get_leaf

--- src/sparse_merkle_tree.rs
+++ src/sparse_merkle_tree.rs
@@ -41,7 +41,7 @@ impl<F: RichField, H: Hasher<F>> SparseMerkleTree<F, H> {
         assert_eq!(path.len(), self.height);
         match self.nodes.get(path) {
             Some(Node::Leaf { value }) => value.clone(),
-            _ => panic!(),
+            _ => vec![F::ZERO; 4],
         }
     }
 
@@ -160,7 +160,7 @@ mod tests {
         let height = 100;
         let mut tree = SparseMerkleTree::<F, H>::new(height);
 
-        for _ in 0..10000 {
+        for _ in 0..100 {
             let index = rng.gen_range(0..1 << height);
             let path = usize_to_vec(index, height);
             let new_leaf = F::rand_vec(4);
@@ -168,6 +168,14 @@ mod tests {
             let proof = tree.prove(&path);
             assert_eq!(tree.get_leaf(&path), new_leaf.clone());
             verify_merkle_proof(new_leaf, index, tree.get_root(), &proof).unwrap();
+        }
+
+        for _ in 0..100 {
+            let index = rng.gen_range(0..1 << height);
+            let path = usize_to_vec(index, height);
+            let leaf = tree.get_leaf(&path);
+            let proof = tree.prove(&path);
+            verify_merkle_proof(leaf, index, tree.get_root(), &proof).unwrap();
         }
     }
 }
